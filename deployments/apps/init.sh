628357ff98a762d24110f48a509fe4dcb0a51ede
shepard <eviltomorrow@163.com>
2023-08-29 22:11:42 +0800

    dev

--- deployments/apps/init.sh
+++ deployments/apps/init.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
 mkdir -p $(pwd)/data/{king-collector,king-email,king-repository}/log
-mkdir -p $(pwd)/data/king-email/etc
-
-# smtp.conf
-cat > $(pwd)/data/king-email/etc/smtp.json <<EOF
-{
-    "server":"mail.liarsa.me",
-    "port":587,
-    "username":"assistant@liarsa.me",
-    "password":"5r6WAmzs2xyGMPqB",
-    "alias":"assistant"
-}
-EOF
 
 touch $(pwd)/data/.ready
--- deployments/db/init.sh
+++ deployments/db/init.sh
@@ -1,57 +1,10 @@
 #!/bin/bash
 
-mkdir -p $(pwd)/data/mongo/{db,conf,log,init}
-mkdir -p $(pwd)/data/mysql/{db,conf,log,init}
+mkdir -p $(pwd)/data/mongo/{db,log,init}
+mkdir -p $(pwd)/data/mysql/{db,log,init}
 mkdir -p $(pwd)/data/etcd
 chmod -R 777 $(pwd)/data
 
-# mongodb.conf
-cat > $(pwd)/data/mongo/conf/mongod.conf <<EOF
-# mongod.conf
-
-# for documentation of all options, see:
-#   http://docs.mongodb.org/manual/reference/configuration-options/
-
-# where to write logging data.
-systemLog:
-  destination: file
-  logAppend: true
-  path: /var/log/mongo/mongod.log
-
-# Where and how to store data.
-storage:
-  dbPath: /var/lib/mongo
-  journal:
-    enabled: true
-#  engine:
-#  wiredTiger:
-
-# how the process runs
-processManagement:
-  # fork: true  # fork and run in background
-  # pidFilePath: /var/run/mongo/mongod.pid  # location of pidfile
-  timeZoneInfo: /usr/share/zoneinfo
-
-# network interfaces
-net:
-  port: 27017
-  bindIp: 0.0.0.0  # Enter 0.0.0.0,:: to bind to all IPv4 and IPv6 addresses or, alternatively, use the net.bindIpAll setting.
-
-
-#security:
-
-#operationProfiling:
-
-#replication:
-
-#sharding:
-
-## Enterprise-Only Options
-
-#auditLog:
-
-#snmp:
-EOF
 
 # init_mongo.js
 cat > $(pwd)/data/mongo/init/init_mongo.js <<EOF
@@ -61,26 +14,6 @@ db.createCollection('metadata');
 db.metadata.createIndex({date: 1, code: 1},{background: true});
 EOF
 chmod a+x $(pwd)/data/mongo/init/init_mongo.js
-
-# my.cnf 
-cat > $(pwd)/data/mysql/conf/my.cnf <<EOF
-[client]
-port = 3306
-default-character-set = utf8
-
-[mysqld]
-user = mysql
-server-id = 1
-port = 3306
-character-set-server = utf8mb4
-authentication_policy = mysql_native_password
-secure_file_priv = /var/lib/mysql
-expire_logs_days = 7
-max_connections = 1000
-log_error = /var/log/mysql/error.log
-socket = /run/mysqld/mysqld.sock
-sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION,PIPES_AS_CONCAT,ANSI_QUOTES'
-EOF
 
 # init.sql
 cat > $(pwd)/data/mysql/init/init_mysql.sql <<EOF
