d74c526a839aacc750d46385258e04df0613b515
shepard <eviltomorrow@gmail.com>
2024-10-09 08:32:11 +0800

    dev

---
+++ apps/king-storage/domain/service/buildmetadata_test.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"testing"
+	"time"
+
+	"github.com/eviltomorrow/king/lib/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildQuoteDayWitchMetadata(t *testing.T) {
+	assert := assert.New(t)
+	data := &model.Metadata{
+		Source:          "sina",
+		Code:            "sh601066",
+		Name:            "中信建投",
+		Open:            22.10,
+		YesterdayClosed: 22.0,
+		Latest:          23.0,
+		High:            24.01,
+		Low:             22.10,
+		Volume:          108202301,
+		Account:         2930241023,
+		Date:            "2024-09-27",
+		Time:            "15:00:03",
+		Suspend:         "正常",
+	}
+
+	date, err := time.Parse(time.DateOnly, "2024-09-27")
+	assert.Nil(err)
+
+	quote, err := BuildQuoteDayWitchMetadata(data, date)
+	assert.Nil(err)
+	assert.NotNil(quote)
+}
--- go.mod
+++ go.mod
@@ -40,6 +40,8 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/agiledragon/gomonkey v2.0.2+incompatible // indirect
+	github.com/agiledragon/gomonkey/v2 v2.11.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/coreos/go-semver v0.3.1 // indirect
