10614cbfb8d3a96825341f9a92cc8f2da95f326a
shepard <eviltomorrow@gmail.com>
2024-10-18 18:05:08 +0800

    dev

---
+++ apps/king-cron/domain/plan/trendfollowing.go
@@ -0,0 +1,30 @@
+package plan
+
+import "github.com/eviltomorrow/king/apps/king-cron/domain"
+
+const (
+	NameWithTrendFollowing  = "CronWithTrendFollowing"
+	AliasWithTrendFollowing = "趋势追踪"
+)
+
+func CronWithTrendFollowing() *domain.Plan {
+	return &domain.Plan{
+		Precondition: func() (domain.StatusCode, error) {
+			return 0, nil
+		},
+		Todo: func(string) (string, error) {
+			return "", nil
+		},
+		WriteToDB: func(string, error) error {
+			return nil
+		},
+
+		NotifyWithData: func(string) error {
+			return nil
+		},
+
+		Status: domain.Ready,
+		Name:   NameWithTrendFollowing,
+		Alias:  AliasWithTrendFollowing,
+	}
+}
