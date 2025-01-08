fb1bee79195302370f6f9f07c19da915a05b69c8
liarsa <eviltomorrow@gmail.com>
2025-01-08 18:10:09 +0800

    dev

--- apps/king-brain/domain/model/m1.go
+++ apps/king-brain/domain/model/m1.go
@@ -36,21 +36,9 @@ func M1(k *chart.K) (*service.Strategy, bool) {
 		fmt.Printf("percent: %v, closed: %v, ma10: %v, ma50: %v, ma150: %v, date: %v\r\n", c.Volatility.PercentageChange, c.Close, c.MA[chart.Ma_10], c.MA[chart.Ma_50], c.MA[chart.Ma150], c.Date.Format(time.DateOnly))
 	}
 
-	var (
-		ma_50 = make([]float64, 0, len(k.Candlesticks))
-		ma150 = make([]float64, 0, len(k.Candlesticks))
-	)
 	for i := 0; i < len(k.Candlesticks); i++ {
 		c := k.Candlesticks[i]
-
-		v_50, ok := c.MA[chart.Ma_50]
-		if ok {
-			ma_50 = append(ma_50, v_50)
-		}
-		v150, ok := c.MA[chart.Ma150]
-		if ok {
-			ma150 = append(ma150, v150)
-		}
+		_ = c
 	}
 	return nil, false
 }
