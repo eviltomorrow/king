0e9157cbe47694c03d98d72227af06cd2068a88a
liarsa <eviltomorrow@gmail.com>
2025-02-06 08:25:28 +0800

    dev

--- apps/king-brain/domain/tactics/buy.go
+++ apps/king-brain/domain/tactics/buy.go
@@ -2,12 +2,12 @@ package tactics
 
 import "github.com/eviltomorrow/king/apps/king-brain/domain/chart"
 
-func TryToFindBuyPoint(k *chart.K) (float64, bool) {
+func TryToFindBuyPoint(k *chart.K) (string, float64) {
 	if len(k.Candlesticks) == 0 {
-		return 0, false
+		return "", 0
 	}
 
 	// last := k.Candlesticks[len(k.Candlesticks)-1]
 
-	return 0, false
+	return "", 0
 }
