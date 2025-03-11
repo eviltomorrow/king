950ad6f5f026ac8790666765ea0ee9d8ba6d8511
liarsa <eviltomorrow@gmail.com>
2025-03-11 18:13:43 +0800

    dev

--- apps/king-brain/domain/model/model_01.go
+++ apps/king-brain/domain/model/model_01.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"fmt"
-
 	"github.com/eviltomorrow/king/apps/king-brain/domain"
 	"github.com/eviltomorrow/king/apps/king-brain/domain/chart"
 )
@@ -16,26 +14,19 @@ func F_01(k *chart.K) (*domain.Plan, error) {
 		return nil, nil
 	}
 
-	days := []int{150}
-	span := 150
-	k.CalMaMany(days)
+	// days := []int{150}
+	// span := 150
+	// k.CalMaMany(days)
 
-	for _, day := range days {
-		data, err := chart.CalculateMaOnNext(k, day, span, 1)
-		if err == chart.ErrNoData {
-			continue
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		closed, err := chart.CalculateClosedOnNext(k, data, 10)
-		if err != nil {
-			return nil, err
-		}
-		fmt.Println(closed)
-
-	}
+	// for _, day := range days {
+	// 	data, err := chart.CalculateMaOnNext(k, day, span, 1)
+	// 	if err == chart.ErrNoData {
+	// 		continue
+	// 	}
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// }
 
 	return &domain.Plan{K: k}, nil
 }
