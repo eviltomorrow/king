f21d8586d03dfee3e20c67b54d184f9e1794a6e1
liarsa <eviltomorrow@gmail.com>
2025-03-12 20:00:12 +0800

    dev

--- apps/king-brain/domain/model/model_01.go
+++ apps/king-brain/domain/model/model_01.go
@@ -1,8 +1,11 @@
 package model
 
 import (
+	"fmt"
+
 	"github.com/eviltomorrow/king/apps/king-brain/domain"
 	"github.com/eviltomorrow/king/apps/king-brain/domain/chart"
+	"github.com/eviltomorrow/king/lib/mathutil"
 )
 
 func init() {
@@ -14,19 +17,81 @@ func F_01(k *chart.K) (*domain.Plan, error) {
 		return nil, nil
 	}
 
-	// days := []int{150}
-	// span := 150
-	// k.CalMaMany(days)
+	days := []int{150}
+	k.CalMaMany(days)
 
-	// for _, day := range days {
-	// 	data, err := chart.CalculateMaOnNext(k, day, span, 1)
-	// 	if err == chart.ErrNoData {
-	// 		continue
-	// 	}
-	// 	if err != nil {
-	// 		return nil, err
-	// 	}
-	// }
-
+	forecastCandlesticskOnNext(k, 200, 3)
 	return &domain.Plan{K: k}, nil
+}
+
+func forecastCandlesticskOnNext(k *chart.K, span, count int) ([]*chart.Candlestick, error) {
+	if len(k.Candlesticks) <= span {
+		return nil, fmt.Errorf("no enough data")
+	}
+
+	var (
+		high = make([]float64, 0, span+count)
+		low  = make([]float64, 0, span+count)
+
+		open   = make([]float64, 0, span+count)
+		closed = make([]float64, 0, span+count)
+	)
+
+	for i := len(k.Candlesticks) - span; i < len(k.Candlesticks); i++ {
+		c := k.Candlesticks[i]
+
+		high = append(high, c.High*100)
+		low = append(low, c.Low*100)
+		open = append(open, c.Open*100)
+		closed = append(closed, c.Close)
+	}
+
+	data, err := chart.CalculateMaToSegment(k, 150)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(data)
+	fClosed, err := forecastPriveOnNext(data[len(data)-1], count)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(fClosed)
+	return nil, nil
+}
+
+func forecastPriveOnNext(data []float64, count int) ([]float64, error) {
+	result := make([]float64, 0, count)
+
+	x := make([]float64, 0, len(data)+count)
+	y := make([]float64, 0, len(data)+count)
+	n := 0
+
+	for _, d := range data {
+		n++
+		x = append(x, float64(n))
+		y = append(y, d)
+	}
+
+	a, b, err := mathutil.LeastSquares(x, y)
+	if err != nil {
+		return nil, err
+	}
+	next := a*float64(n+1) + b
+	result = append(result, mathutil.Trunc4(next))
+
+	for i := 1; i < count; i++ {
+		y = append(y, next)
+		y = y[1:]
+
+		a, b, err = mathutil.LeastSquares(x, y)
+		if err != nil {
+			return nil, err
+		}
+
+		next = a*float64(n+1) + b
+		result = append(result, mathutil.Trunc4(next))
+	}
+	return result, nil
 }
--- apps/king-brain/domain/model/model_01_test.go
+++ apps/king-brain/domain/model/model_01_test.go
@@ -15,8 +15,8 @@ func TestF01(t *testing.T) {
 	assert := assert.New(t)
 
 	stock := &data.Stock{
-		Code: "sh601933",
-		// Code: "sh688256",
+		// Code: "sh601933",
+		Code: "sh688256",
 		Name: "--",
 	}
 	date := time.Date(2025, time.March, 10, 12, 0, 0, 0, time.Local)
