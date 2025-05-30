4cb22c67dedb497559ba90b9aebec3e76f787a38
liarsa <eviltomorrow@gmail.com>
2025-05-30 10:57:46 +0800

    dev

---
+++ lib/system/path.go
@@ -0,0 +1,12 @@
+package system
+
+import "path/filepath"
+
+func ResetPath(baseDir, path string) string {
+	if path != "" {
+		if !filepath.IsAbs(path) {
+			path = filepath.Join(baseDir, path)
+		}
+	}
+	return path
+}
