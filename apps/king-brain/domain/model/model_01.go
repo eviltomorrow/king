989f3bbb70677d6381653a517c0dda136afc44ca
liarsa.me <shepard@liarsa.me>
2025-06-08 22:36:05 +0800

    dev

--- apps/king-brain/domain/model/model_01.go
+++ apps/king-brain/domain/model/model_01.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+
 	"github.com/eviltomorrow/king/apps/king-brain/domain"
 	"github.com/eviltomorrow/king/apps/king-brain/domain/chart"
 )
@@ -17,16 +19,11 @@ func F_01(k *chart.K) (*domain.Plan, error) {
 	days := []int{10, 20, 50, 150, 200}
 	k.CalMaMany(days)
 
-	var (
-		cache = make([]*chart.Candlestick, 0, len(k.Candlesticks))
-		next  = make([]*chart.Candlestick, 0, 3)
-	)
-	for _, c := range k.Candlesticks {
-		cache = append(cache, c)
-		if len(cache) == 0 {
-			continue
-		}
-		_ = next
+	segment, err := chart.CalculateMaToSegment(k, 150)
+	if err != nil {
+		return nil, err
 	}
+
+	fmt.Println(segment)
 	return &domain.Plan{K: k}, nil
 }
